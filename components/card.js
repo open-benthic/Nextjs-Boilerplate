5891fddf6f3d635d1b12d900024f38c2dfc401cf
omic alpha3 <omicalpha3@gmail.com>
2020-10-05 19:01:44 +0330

    minor style changes

--- components/card.js
+++ components/card.js
@@ -6,19 +6,19 @@ const StyledCard = styled.div`
   text-align: left;
   color: ${({ theme }) => theme.colors.dark};
   text-decoration: none;
-  border: 1px solid #eaeaea;
-  
-  
-  border-radius: 11px;
+  border: 2px solid #eaeaea;
+
+  border-radius: 10px;
   transition: 0.3s ease-out;
   transform: translateY(0px);
   box-shadow: ${({ theme }) => theme.shadows.primary};
   &:hover {
-    transform: translateY(-5px);
-    transition: ease-out 0.2s;
+    transform: translateY(-3px);
+    transition: ease-out transform 0.25s;
   }
   &:focus {
     transform: translateY(-5px);
+    border: 2px solid rgb(12, 60, 155);
     transition: ease-out 0.2s;
   }
 `;
